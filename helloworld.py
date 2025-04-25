c4dce4a84cc862798d820544172be76e7696562b
Andrew James Okpainmo <okpainmoandrew@gmail.com>
2025-04-25 13:57:13 +0100

    chore: update to helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,4 @@ print("An update to initiate the jenkins Poll SCM trigger")
 
 print("Updating, to trigger a change via a pipeline job")
 
-print("A final to trigger a change via a pipeline job")
 
-print("A final to trigger a change via a pipeline job - found a potential issue for the trigger failure")
