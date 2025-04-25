18b372a8b975aae321c5c756f2f3f0384af89084
Andrew James Okpainmo <okpainmoandrew@gmail.com>
2025-04-25 13:31:30 +0100

    chore: update to helloworld.py - I guess I found the polling failure issue

--- helloworld.py
+++ helloworld.py
@@ -5,3 +5,5 @@ print("An update to initiate the jenkins Poll SCM trigger")
 print("Updating, to trigger a change via a pipeline job")
 
 print("A final to trigger a change via a pipeline job")
+
+print("A final to trigger a change via a pipeline job - found a potential issue for the trigger failure")
